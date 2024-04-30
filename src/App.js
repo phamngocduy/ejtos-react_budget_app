04a30c71f0760478a2faa047de980739b5769efc
phamngocduy <phmngcduy@gmail.com>
2024-04-30 01:03:57 -0400

    Add Budget

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Code to import Budget.js
+// Code to import Budget.js
 import Budget from './components/Budget';
-
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -15,23 +13,29 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                    {
+                        // Budget component
+                        <div className='col-sm'>
+                            <Budget/>
+                        </div>
+                    }        
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                    {
+                        // Remaining component
+                        <div className="col-sm">
+                            <Remaining/>
+                        </div>
+                    }        
 
-                        {
+                    {
                             /* Add ExpenseTotal component here */
                         }        
                        
-                        {
+                    {
                             /* Add ExpenseList component here */
                         }         
 
-                        {
+                    {
                             /* Add ExpenseItem component here */
                         }        
 
